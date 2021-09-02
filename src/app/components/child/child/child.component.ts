631c6c0bcc551979891b930fa3f53f1e7f48c2f2
ธนโชติ ศักดิ์ธรรมเจริญ <pet.14336@gmail.com>
2021-09-02 21:55:33 +0700

    first commit

--- src/app/components/child/child/child.component.ts
+++ src/app/components/child/child/child.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component,  Input } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core'
+import { OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked,OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
 
   @Input() parentDataChild!: number;
 
@@ -14,11 +22,34 @@ export class ChildComponent implements OnInit {
 
   childData!: number;
 
-  constructor() { }
+  constructor() { console.log('Constructor Work!'); }
+  ngDoCheck(): void {
+    console.log('ngDoCheck Works')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit Works')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked Works')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit Works')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked Works')
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy Works')
+  }
+
+  ngOnChanges(){
+    console.log('ngOnChanges Works')
+  }
 
   ngOnInit(): void {
     this.childData = 0;
     this.messageEvent.emit(this.childData)
+    console.log('ngOnInit Works')
   }
 
   onClick2Parent(){
@@ -26,6 +57,6 @@ export class ChildComponent implements OnInit {
     this.messageEvent.emit(this.childData)
   }
 
-  
+
 
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,27 +1,83 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ChildComponent } from '../child/child/child.component';
+import { OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked,OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   @ViewChild(ChildComponent)
   childComponent!: ChildComponent;
+  
+  textMessage!:string;
+  status!: string;
 
+  showChild!: boolean;
   
   parentData!: number;
   ChildData!: number;
-  constructor() { }
+  constructor() { console.log('Constructor Work!'); }
+  ngDoCheck(): void {
+    console.log(this.textMessage)  
+    console.log('ngDoCheck Works')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit Works')
+  }
+  ngAfterContentChecked(): void {
+
+    //change this ID to Your ID please
+    if(this.textMessage == "B6309343"){
+      this.status = "Your ID is Ok"
+    }else{
+      this.status = "Error"
+    }
+    console.log('ngAfterContentChecked Works')
+  }
+
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit Works')
+  }
+
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked Works')
+  }
+
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy Works')
+  }
+
+
+
+  ngOnChanges(){
+    console.log('ngOnChanges Works')
+  }
+
 
   ngOnInit(): void {
     this.parentData = 0;
+    this.ChildData = 0;
+    console.log('ngOnInit Works')
   }
+
+
   onClick2Child(){
     this.parentData += 1;
   }
+
 
   receiveData($event: number){
     this.ChildData = $event;
@@ -29,6 +85,11 @@ export class ParentComponent implements OnInit {
 
   onClickViewChild(){
     this.childComponent.onClick2Parent();
+  }
+
+  update(){
+    this.showChild = !this.showChild;
+    
   }
 
 }
